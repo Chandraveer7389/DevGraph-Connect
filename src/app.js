4d12216df9af3159ef18339f2e13cf84987ef5eb
Chandraveer <chandraveer7389@gmail.com>
2026-02-08 12:29:53 +0530

    Writing Auth Middle Ware

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use(
-  "/",
-  [(req, res, next) => {
-    console.log("Handler 1")
-    res.send("Res 1");
+app.use("/admin", (req, res, next) => {
+  let token = "ab";
+  if (token === "a") {
     next();
-  }],
-  (req, res, next) => {
-    console.log("Handler 2")
-    //res.send("res 2");
-    next();
-  },
-);
+  } else {
+    res.status(404).send("Not authorized");
+  }
+});
+
+app.delete("/admin/deleteData", (req, res) => {
+  res.send("Data deleted");
+});
+app.get("/admin/getData", (req, res) => {
+  res.send("user data");
+});
+
 app.listen(7000, () => {
   console.log("Server listening at the port");
 });
