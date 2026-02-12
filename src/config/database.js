3d59ea833c181d7b9c915bb9a3f1ae505d10e00d
Chandraveer <chandraveer7389@gmail.com>
2026-02-12 11:10:58 +0530

    Minor fixes

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://chandraveerDevGraph:Vvv1998@cluster0.wowksmd.mongodb.net/devTinder?appName=Cluster0")
+    await mongoose.connect("")
 }
 
 module.exports = connectDB;
